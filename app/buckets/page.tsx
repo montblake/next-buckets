161be73d42d18dd44f51a88a7b28f6403cea11a3
Blake Montgomery <blakemontgomery312@gmail.com>
2024-01-31 11:52:09 -0600

    Working: Location of Search was the ISSUE

--- app/buckets/page.tsx
+++ app/buckets/page.tsx
@@ -1,4 +1,28 @@
+<<<<<<< HEAD
 import Buckets from '@/ui/components/buckets-reducer';
+=======
+import Buckets from '@/ui/components/buckets';
+import { Suspense } from 'react';
+import Pagination from '@/ui/components/pagination';
+import { BucketsSkeleton } from '@/ui/components/skeletons';
+import { fetchBucketsPages } from '@/lib/data';
+import Search from '@/ui/components/search';
+
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  }
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchBucketsPages(query);
+  console.log("TOTAL PAGES", totalPages);
+>>>>>>> 77bdf12 (Working: Location of Search was the ISSUE)
 
 export default function Page() {
   return (
